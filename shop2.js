e44964f39787329beb2d37b44034df0d8cbe3e4c
nikitgondhali <nikitgondhali101@gmail.com>
2019-10-02 00:16:55 +0530

    publishing my website
    happy

---
+++ shop2.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Phone number should be a 10 digit number.<br> Do not prefix +91 or 0";
+}
+function myFunction1() {
+  document.getElementById("demo1").innerHTML = "Phone number should be a 10 digit <br>number. Do not prefix +91 or 0";
+}
